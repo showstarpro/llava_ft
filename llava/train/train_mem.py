a67510eefcfcb6aff4f9a1f45dd48bcda7c81ce0
showstarpro <1359947339@qq.com>
2024-11-29 07:09:12 +0000

    add wandb key

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -1,4 +1,6 @@
 from llava.train.train import train
+import os
 
+os.environ["WANDB_API_KEY"] = '7352f9a349b74e01062672d0bc0bd3a8094677e2' 
 if __name__ == "__main__":
     train(attn_implementation="flash_attention_2")
