ceedde028468aba68b0f43104b25072c2924cc18
showstarpro <1359947339@qq.com>
2024-12-18 09:55:51 +0000

    delet useless code

--- llava/train/train_contr.py
+++ llava/train/train_contr.py
@@ -1064,9 +1064,13 @@ def train(attn_implementation=None):
                     tokenizer=tokenizer,
                     args=training_args,
                     **data_module)
-
+    
+    trainer.save_state()
     safe_save_model_for_hf_trainer(trainer=trainer,
                                     output_dir=training_args.output_dir)
+    
+    print('hello')
+    return
 
     if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
         trainer.train(resume_from_checkpoint=True)
