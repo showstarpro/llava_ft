926132e4c17c45a2ae2a96cd04ee72005209250d
showstarpro <1359947339@qq.com>
2024-12-20 01:00:43 +0000

    udpate eval.sh

--- eval.sh
+++ eval.sh
@@ -1,36 +1,17 @@
-
 #!/bin/bash
 
-gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
-IFS=',' read -ra GPULIST <<< "$gpu_list"
+python -m llava.eval.model_vqa_loader \
+    --model-path /lpai/volumes/so-volume-ga/lhp/llava-v1.5/vicuna-7b-v1.5-pretrain/llava-v1.5-7b-clip-vitl-336-base \
+    --question-file ./playground/data/eval/MME/llava_mme.jsonl \
+    --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
+    --answers-file ./playground/data/eval/MME/answers/llava-v1.5-7b-clip-vitl-336-base.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
 
-CHUNKS=${#GPULIST[@]}
+cd ./playground/data/eval/MME
 
-CKPT="llava-v1.5-7b-clip-vitl-336"
-SPLIT="llava_vqav2_mscoco_test-dev2015"
+python convert_answer_to_mme.py --experiment llava-v1.5-7b-clip-vitl-336-base
 
-# for IDX in $(seq 0 $((CHUNKS-1))); do
-#     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
-#         --model-path /lpai/volumes/so-volume-ga/lhp/llava-v1.5/vicuna-7b-v1.5-pretrain/llava-v1.5-7b-clip-vitl-336-base \
-#         --question-file ./playground/data/eval/vqav2/$SPLIT.jsonl \
-#         --image-folder ./playground/data/eval/vqav2/test2015 \
-#         --answers-file ./playground/data/eval/vqav2/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl \
-#         --num-chunks $CHUNKS \
-#         --chunk-idx $IDX \
-#         --temperature 0 \
-#         --conv-mode vicuna_v1 &
-# done
+cd eval_tool
 
-wait
-
-output_file=./playground/data/eval/vqav2/answers/$SPLIT/$CKPT/merge.jsonl
-
-# Clear out the output file if it exists.
-> "$output_file"
-
-# Loop through the indices and concatenate each file.
-for IDX in $(seq 0 $((CHUNKS-1))); do
-    cat ./playground/data/eval/vqav2/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl >> "$output_file"
-done
-
-python convert_vqav2_for_submission.py --split $SPLIT --ckpt $CKPT
+python calculation.py --results_dir answers/llava-v1.5-7b-clip-vitl-336-base
