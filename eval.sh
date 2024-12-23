5c56baa23519c174ddef5e2f9987a07d076851f6
showstarpro <1359947339@qq.com>
2024-12-23 04:33:12 +0000

    update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-python -m llava.eval.model_vqa \
-    --model-path /lpai/volumes/so-volume-ga/lhp/llava-v1.5/vicuna-7b-v1.5-pretrain/llava-v1.5-7b-clip-vitl-336-control-v4/llava-1.5-7b \
-    --question-file ./playground/data/eval/mm-vet/llava-mm-vet.jsonl \
-    --image-folder ./playground/data/eval/mm-vet/images \
-    --answers-file ./playground/data/eval/mm-vet/answers/llava-v1.5-7b-clip-vitl-336-control-v4.jsonl \
+CKPT='/lpai/volumes/so-volume-ga/lhp/llava-v1.5/vicuna-7b-v1.5-pretrain/llava-v1.5-7b-clip-vitl-336-control-v13'
+SPLIT="mmbench_dev_cn_20231003"
+
+python -m llava.eval.model_vqa_mmbench \
+    --model-path $CKPT \
+    --question-file ./playground/data/eval/mmbench_cn/$SPLIT.tsv \
+    --answers-file ./playground/data/eval/mmbench_cn/answers/$SPLIT/llava-v1.5-7b-clip-vitl-336-control-v13.jsonl \
+    --lang cn \
+    --single-pred-prompt \
     --temperature 0 \
     --conv-mode vicuna_v1
 
-mkdir -p ./playground/data/eval/mm-vet/results
+mkdir -p playground/data/eval/mmbench/answers_upload/$SPLIT
 
-python scripts/convert_mmvet_for_eval.py \
-    --src ./playground/data/eval/mm-vet/answers/llava-v1.5-7b-clip-vitl-336-control-v4.jsonl \
-    --dst ./playground/data/eval/mm-vet/results/llava-v1.5-7b-clip-vitl-336-control-v4.json
+python scripts/convert_mmbench_for_submission.py \
+    --annotation-file ./playground/data/eval/mmbench_cn/$SPLIT.tsv \
+    --result-dir ./playground/data/eval/mmbench_cn/answers/$SPLIT \
+    --upload-dir ./playground/data/eval/mmbench_cn/answers_upload/$SPLIT \
+    --experiment llava-v1.5-7b-clip-vitl-336-control-v13
+
+
